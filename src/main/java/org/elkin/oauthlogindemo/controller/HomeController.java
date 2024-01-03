3f4fff7ecf007365329cde5e179e69343bafa605
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2024-01-03 16:28:39 +0300

    add logout method in controller

--- src/main/java/org/elkin/oauthlogindemo/controller/HomeController.java
+++ src/main/java/org/elkin/oauthlogindemo/controller/HomeController.java
@@ -1,5 +1,10 @@
 package org.elkin.oauthlogindemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,14 @@ public class HomeController {
     @GetMapping("/secured")
     public String secured(){
         return "Hello, Secured!";
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        SecurityContextHolder.getContext().setAuthentication(null);
     }
 }
